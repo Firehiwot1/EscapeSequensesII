60acacc446d251954d31e06b81da4fe4bec7e7aa
Firehiwot1 <fmazengia@gmail.com>
2019-06-13 13:34:20 -0400

    escapeII commit

---
+++ src/EscapeSeq.java
@@ -0,0 +1,11 @@
+public class EscapeSeq {
+    public static void main (String[] args){
+        System.out.print("Cost \t   Quantity \t  Total \n");
+        System.out.print(" $1000.00 \t  4 \t $4000 \n " );
+        System.out.print("  $50.00 \t   8  \t $400\n " );
+        System.out.print(" Total : \t  12  \t $4,400\n " );
+
+
+
+    }
+}
